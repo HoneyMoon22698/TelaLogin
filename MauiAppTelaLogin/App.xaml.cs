668a48b3106ec940212b04cd64317ac96d6574ed
HoneyMoon22698 <2404872@escolas.anchieta.br>
2025-03-03 22:12:26 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTelaLogin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTelaLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppTelaLogin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTelaLogin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
